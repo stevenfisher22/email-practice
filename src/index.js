33a07ac00ef950b423be1b042a5353212385d4e7
Steven Fisher <steven.fisher22@gmail.com>
2019-03-08 12:56:52 -0600

    components set up and props working

--- src/index.js
+++ src/index.js
@@ -2,12 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function App() {
+function Email(props) {
     return (
         <div>
-            Test
+            <Sender sender={props.info.email1.sender}/>
+            <Subject subject={props.info.email1.subject}/>
+            <Date date={props.info.email1.date}/>
+            <EmailText emailText={props.info.email1.emailText}/>
         </div>
     )
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+const Sender = (props) => {
+    return (
+        <div>
+            {props.sender}
+        </div>
+    )
+}
+const Subject = (props) => {
+    return (
+        <div>
+            {props.subject}
+        </div>
+    )
+}
+const Date = (props) => {
+    return (
+        <div>
+            {props.date}
+        </div>
+    )
+}
+const EmailText = (props) => {
+    return (
+        <div>
+            {props.emailText}
+        </div>
+    )
+}
+
+var info = {
+    email1: {
+        sender: 'Steven Fisher',
+        subject: 'Would you like to go to the mall later?',
+        date: 'Mar 8',
+        emailText: 'I guess I laid it all out there in the subject, huh?'
+    }
+}
+
+ReactDOM.render(<Email info={info}/>, document.querySelector('#root'))
